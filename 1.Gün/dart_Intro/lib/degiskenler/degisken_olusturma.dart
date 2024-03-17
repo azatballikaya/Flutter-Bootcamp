1a5983588baeff98326f9c869fcac08e3a181903
azatballikaya <azat.bal@hotmail.com>
2024-03-17 14:47:40 +0300

    1.Gün ödev öncesi

---
+++ 1.Gün/dart_Intro/lib/degiskenler/degisken_olusturma.dart
@@ -0,0 +1,27 @@
+void main(){
+  var ogrenciAdi="Ahmet";
+  var ogrenciYas=23;
+  var ogrenciBoy=1.78;
+  var ogrenciBasHarf='A';
+  var ogrenciDevamEdiyorMu=true;
+  print(ogrenciAdi);
+  print(ogrenciYas);
+  print(ogrenciBoy);
+  print(ogrenciBasHarf);
+  print(ogrenciDevamEdiyorMu);
+
+  int urun_id=3416;
+  String urun_adi="Kol Saati";
+  int urun_adet=100;
+  double urun_fiyat=149.99;
+  String urun_tedarikci="Rolex";
+  print("Ürün id: $urun_id");
+  print("Ürün adı: $urun_adi");
+  print("Ürün adet: $urun_adet");
+  print("Ürün fiyatı: $urun_fiyat ₺");
+  print("Ürün tedarikçi: $urun_tedarikci ");
+  int a =100;
+  int b = 200;
+  print("$a + $b = ${a+b}");
+
+}
---
+++ 1.Gün/dart_Intro/lib/degiskenler/sabitler.dart
@@ -0,0 +1,9 @@
+void main(){
+  final int numara=100;
+  print(numara);
+
+  const int sonuc=500;
+  print(sonuc);
+  
+
+}
